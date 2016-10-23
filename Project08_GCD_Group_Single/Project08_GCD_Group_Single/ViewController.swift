7aa0f510d856b223ac884ab2b6948f6c08f90d4f
lfs736012 <lfs736012@163.com>
2016-10-23 21:38:19 +0800

    gcd 分组

--- Project08_GCD_Group_Single/Project08_GCD_Group_Single/ViewController.swift
+++ Project08_GCD_Group_Single/Project08_GCD_Group_Single/ViewController.swift
@@ -12,24 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.downBtn.isHidden = false
     }
     
     func beginGroup(){
-        let queue = DispatchQueue.global()
-        let groupQueue = DispatchGroup()
+        //新建队列分组
+//        let groupQueue = DispatchGroup()
+//        
+//        //把任务分组异步执行
+//        DispatchQueue.global().async(group: groupQueue, execute: {
+//            sleep(2)
+//            print("A任务执行完毕" +  String(describing: Thread.current))
+//        })
+//        
+//        DispatchQueue.global().async(group: groupQueue, execute: {
+//            sleep(2)
+//            print("B任务执行完毕" +  String(describing: Thread.current))
+//        })
+        
+        
         
     }
 
     // MARK: 懒加载
+    /// 下载按钮
     lazy var downBtn: UIButton = {
         let btn = UIButton(type: .custom)
-        btn.frame = CGRect(x: 20, y: 50, width: 60, height: 30)
-        btn.backgroundColor = UIColor.purple
         btn.setTitle("开始下载", for: .normal)
         btn.setTitleColor(UIColor.black, for: .normal);
-        btn.backgroundColor = UIColor.clear
+        btn.setTitle("高亮状态", for: .highlighted)
+        btn.setTitleColor(UIColor.blue, for: .highlighted)
+        btn.backgroundColor = UIColor.purple
         btn.addTarget(self, action: #selector(beginGroup), for: .touchUpInside)
+        self.view.addSubview(btn)
+        btn.snp.makeConstraints({ (make) in
+            make.top.equalTo(20)
+            make.centerX.equalTo(self.view)
+            make.height.equalTo(50)
+        })
         return btn
     }()
 
