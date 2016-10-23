45e30e750b3f1bcc20ccd3803976c606f29d3202
lfs736012 <lfs736012@163.com>
2016-10-23 20:16:43 +0800

    gcd 图纸下载

--- Project07_GCDDownImages/Project07_GCDDownImages/ViewController.swift
+++ Project07_GCDDownImages/Project07_GCDDownImages/ViewController.swift
@@ -12,12 +12,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.downBtn.isHidden = false
         
     }
     // MARK: private Method
     func downLoadImage(){
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
         
+        var imageList = Array<UIImage>()
+        
+        let queue = DispatchQueue(label: "AsyncConcurrent", attributes:DispatchQueue.Attributes.concurrent)
+        for url in self.imageURLs {
+            queue.async {
+                let data = NSData.init(contentsOf: url)
+                let img = UIImage.init(data: data as! Data)
+                imageList.append(img!)
+            }
+        }
+        
+        queue.async(flags: .barrier){//添加隔断
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+                var i = 0
+                for image in imageList {
+                    let iv = self.imageViewList[i]
+                    iv.image = image
+                    i += 1
+                }
+            }
+        }
     }
     
     // MARK: 懒加载
@@ -34,14 +57,30 @@ class ViewController: UIViewController {
     }()
     lazy var imageViewList:Array<UIImageView> = {
         var arr = [UIImageView]()
-        
-        for i in self.imageURLs {
+        var i = 0
+        var lastView = UIView()
+        for _ in self.imageURLs {
+            
             let iv = UIImageView()
             iv.contentMode = UIViewContentMode.scaleAspectFit
             self.view.addSubview(iv)
             iv.snp.makeConstraints({ (make) in
-                
+                if (i == 0){
+                    make.top.equalTo(self.downBtn.snp.bottom).offset(20)
+                    make.left.equalTo(20)
+                    make.right.equalTo(-20)
+                }else{
+                    make.top.equalTo(lastView.snp.bottom).offset(20)
+                    make.size.equalTo(lastView)
+                    if (i == self.imageURLs.count - 1){
+                        make.bottom.equalTo(-20)
+                    }
+                }
             })
+            lastView = iv
+            arr.append(iv)
+            
+            i += 1
         }
         
         return arr
@@ -49,13 +88,18 @@ class ViewController: UIViewController {
     /// 下载按钮
     lazy var downBtn: UIButton = {
         let btn = UIButton(type: .custom)
-        btn.frame = CGRect(x: 20, y: 50, width: 60, height: 30)
         btn.setTitle("开始下载", for: .normal)
         btn.setTitleColor(UIColor.black, for: .normal);
         btn.setTitle("高亮状态", for: .highlighted)
         btn.setTitleColor(UIColor.blue, for: .highlighted)
-        btn.backgroundColor = UIColor.clear
+        btn.backgroundColor = UIColor.purple
         btn.addTarget(self, action: #selector(downLoadImage), for: .touchUpInside)
+        self.view.addSubview(btn)
+        btn.snp.makeConstraints({ (make) in
+            make.top.equalTo(20)
+            make.centerX.equalTo(self.view)
+            make.height.equalTo(50)
+        })
         return btn
     }()
 
